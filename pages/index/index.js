71a82de2c5471fe086a0117bbb506755e76adca4
QiankunH1 <1821330332@qq.com>
2018-09-06 21:58:41 +0800

    9/6星期四

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,6 +6,7 @@ const app = getApp()
 Page({
   data: {
     swiperData: [], 
+    mainContent: [],
     indicatorDots:true,
     autoplay: false,
     interval: 1000,
@@ -13,42 +14,110 @@ Page({
     indicindicatorActiveColor:"green",
     circular:true,
     indicatorColor: "blue",
-    isLoading:false
+    isLoading:false,
+    pn:1,
+    hasMore:true,
+    loadDone:false
   },
   //事件处理函数
 
   onLoad: function () {
     this.getData();
     this.getContent();
+  
     } ,
     
   getData(){
-    this.setData({
-      isLoading:true
-    })
-   fetch.get('/swiper').then(res=>{
-     this.setData({
-       swiperDate:res.data,
-       isLoading: false
-     })  
-   })
-  },
-  getContent(){
-    fetch.get('/category/books').then(res=>{
-      // console.log(res)
+    return new Promise((resolve,reject)=>{
       this.setData({
-        mainContent:res.data
+        isLoading: true
+      })
+      fetch.get('/swiper').then(res => {
+        resolve()
+        // console.log(res)
+        this.setData({
+          swiperDate: res.data,
+          isLoading: false,
+        })
+      }).catch(err => {
+        reject(reject)
+        this.setData({
+          isLoading: false
+        })
       })
     })
   },
+  getContent(){
+    return new Promise((resolve, reject) => {
+      this.setData({
+        isLoading: true
+      })
+      fetch.get('/category/books').then(res => {
+        resolve()
+        this.setData({
+          mainContent: res.data,
+          isLoading: false
+        })
+      }) 
+    })
+  },
+  getAllData(){
+    return new Promise((resolve,reject)=>{
+      Promise.all([this.getData(), this.getContent()]).then(res=>{
+        resolve() 
+      }).catch(err=>{
+        reject(reject)
+      })
+    })
+
+  },
   jumpBook(event){
-    // console.log(event)
+  
     const id = event.currentTarget.dataset.id
     wx.navigateTo({
       url: `/pages/details/details?id=${id}`
     })
-  }
- 
+  },
+  getMoreContent(){
+    return new Promise(resolve=>{
+      fetch.get('/category/books', { pn: this.data.pn }).then(res => {
+        console.log(res)
+        let newArr=[...this.data.mainContent,...res.data]
+        this.setData({
+          mainContent:newArr,
+          isLoading: false
+        })
+        resolve(res)
+      }) 
+    })
+    
+  },
+  onPullDownRefresh: function () {
+    console.log("下拉执行了")
+      this.getAllData().then(()=>{
+        this.setData({
+          pn: 1,
+          hasMore: true,
+        })
+        wx.stopPullDownRefresh()  
+      })
+  },
+ onReachBottom(){
+   console.log("上拉执行了")
+   if(this.data.hasMore){
+     this.setData({
+       pn:this.data.pn+1,
+       loadDone:true
+     })
+     this.getMoreContent().then(res => {
+       if (res.data.length < 2) {
+         this.setData({
+           hasMore: false,
+         })
+       }
+     })
+   }
+ }
 })
 
  
